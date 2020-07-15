8571ba3514fffe920127fab49ac40f1f51316af9
denglinhua <deng.linhua@datasw.com>
2020-07-15 16:31:21 +0800

    update 更新eslint配置

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,17 +1,34 @@
 module.exports = {
   root: true,
   env: {
+    es6: true,
     node: true,
   },
   extends: [
+    'eslint:recommended',
     'plugin:vue/essential',
     '@vue/airbnb',
   ],
   parserOptions: {
     parser: 'babel-eslint',
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+    }
   },
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
   },
+  overrides: [
+    {
+      files: [
+        '**/__tests__/*.{j,t}s?(x)',
+        '**/tests/unit/**/*.spec.{j,t}s?(x)'
+      ],
+      env: {
+        jest: true
+      }
+    }
+  ]
 };
